8c4acd63f9223175673d099684d7fbbe96e135a7
DmtrBoyko <heallolpro@mail.ru>
2024-07-18 16:36:25 +0300

    После изменения файлов перестал подключаться к базе данных

--- pages/db.php
+++ pages/db.php
@@ -5,11 +5,11 @@ $passwd = '1234';
 $dbname = 'RegUser';
 $chrs = 'utf8mb4';
 $attr = "mysql:host=$servername;dbname=$dbname,charset=$chrs";
-//l = [
-//    PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
-//    PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
-//    PDO::ATTR_EMULATE_PREPARES    => false,
-//];
+$opts = [
+    PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES    => false,
+];
 
 //$db_connect = mysqli_connect($servername, $username, $passwd, $dbname);
 //$db_connect = mysqli_connect('localhost:3306', 'root', '1234', 'RegUser');
--- pages/registration.php
+++ pages/registration.php
@@ -1,51 +1,52 @@
+
 <?php
 
 require_once 'db.php';
 
-//try {
-$pdo = new PDO($attr, $username, $passwd, $opts);
-//}
-//catch (PDOException $e) {
-//    throw new PDOException($e->getMessage(), (int)$e->getCode());
-//}
+try {
+    $pdo = new PDO($attr, $username, $passwd, $opts);
+}
+catch (PDOException $e) {
+    throw new PDOException($e->getMessage(), (int)$e->getCode());
+}
 
 
 //$db_connect = mysqli_connect('localhost', 'root', '1234', 'RegUser');
-if (!$pdo) {
-    die ('Подключиться не удалось'. mysqli_connect_error());
+if (!(require_once 'db.php')) {
+//    die ('Подключиться не удалось'. mysqli_connect_error());
     echo "Не могу подключиться к базе данных";
 }else{
     echo "Подключился к базе данных";
 }
 
-$login = $_POST['login']; #получаем логин с помощью метода POST все как и снизу
-$email = $_POST['email'];
-$password = $_POST['password'];
-$reppswd = $_POST['reppswd'];
+//$login = $_POST['login']; #получаем логин с помощью метода POST все как и снизу
+//$email = $_POST['email'];
+//$password = $_POST['password'];
+//$reppswd = $_POST['reppswd'];
 
 
-if ($password != $reppswd) {
-    echo "Пароли не совпадают!";
-}else{
-    echo "Пароли совпадают";
-    $login_check= "SELECT * FROM users WHERE login='$login'";
-    $email_check="SELECT * FROM users WHERE e_mail='$email'";
-    echo "Проверка прошла";
-    if (mysqli_num_rows(mysqli_query($db_connect, $login_check)) > 0) {
-        header("Location: login_failed.php");
-        echo "Логин уже занят";
-    }elseif(mysqli_num_rows(mysqli_query($db_connect, $email_check)) > 0) {
-        echo "Логин не занят";
+//if ($password != $reppswd) {
+//    echo "Пароли не совпадают!";
+//}else{
+//    echo "Пароли совпадают";
+//    $login_check= "SELECT * FROM users WHERE login='$login'";
+//    $email_check="SELECT * FROM users WHERE e_mail='$email'";
+//    echo "Проверка прошла";
+//    if (mysqli_num_rows($pdo->query($login_check)) > 0) {
+//        header("Location: login_failed.php");
+//        echo "Логин уже занят";
+//    }elseif(mysqli_num_rows(mysqli_query($pdo->query($email_check)) > 0) {
+//        echo "Логин не занят";
 //	$email_check="SELECT * FROM users WHERE e_mail='$email'";
 //	if (mysqli_num_rows(mysqli_query($db_connect, $email_check)) > 0) {
-	header("Location: email_failed.php");
-	echo "email уже занят";
-    }else{
-        $insrt = "INSERT INTO users (e_mail, login, password) VALUES ('$email', '$login', '$password');";
-        $result = mysqli_query($db_connect, $insrt);
-        echo "Данные вставлены";
-    }
-}
+//	header("Location: email_failed.php");
+//	echo "email уже занят";
+//    }else{
+//        $insrt = "INSERT INTO users (e_mail, login, password) VALUES ('$email', '$login', '$password');";
+//        $result = $pdo->query($insrt);
+//        echo "Данные вставлены";
+//    }
+//}
 
 ?>
 
