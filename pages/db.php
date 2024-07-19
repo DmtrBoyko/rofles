fb88f07182407c3661f57c9231390b0b1777b2a9
DmtrBoyko <heallolpro@mail.ru>
2024-07-19 13:32:31 +0300

    Изменил метод регистрации, начал делать процесс проверки логина и почты в базе данных

--- pages/db.php
+++ pages/db.php
@@ -4,12 +4,12 @@ $username = 'root';
 $passwd = '1234';
 $dbname = 'RegUser';
 $chrs = 'utf8mb4';
-$attr = "mysql:host=$servername;dbname=$dbname,charset=$chrs";
-$opts = [
-    PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES    => false,
-];
+$attr = "mysql:host=$servername;dbname=$dbname";
+//$opts = [
+//    PDO::ATTR_ERRMODE             => PDO::ERRMODE_EXCEPTION,
+//    PDO::ATTR_DEFAULT_FETCH_MODE  => PDO::FETCH_ASSOC,
+//    PDO::ATTR_EMULATE_PREPARES    => false,
+//];
 
 //$db_connect = mysqli_connect($servername, $username, $passwd, $dbname);
 //$db_connect = mysqli_connect('localhost:3306', 'root', '1234', 'RegUser');
