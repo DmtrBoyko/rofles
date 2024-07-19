14874bbec114a1671db59d571bfe8599bc619e9a
DmtrBoyko <heallolpro@mail.ru>
2024-07-19 17:23:19 +0300

    Доделал регистрацию (кроме окна авторизации после ее завершения, разбил по функциям

--- pages/registration.php
+++ pages/registration.php
@@ -13,97 +13,78 @@ try {
 } catch (PDOException $e) {
     die("Не удалось подключиться к БД: " . $e->getMessage());
 }
-//catch (PDOException $e) {
-//    throw new PDOException($e->getMessage(), (int)$e->getCode());
-//}
 
 $login = $_POST['login']; #получаем логин с помощью метода POST все как и снизу
 $email = $_POST['email'];
 $password = $_POST['password'];
 $reppswd = $_POST['reppswd'];
 
-<<<<<<< HEAD
-//$db_connect = mysqli_connect('localhost', 'root', '1234', 'RegUser');
-//    if (require_once 'db.php') {
-//    die ('Подключиться не удалось'. mysqli_connect_error());
-//    echo "Подключился к БД";
-//}else{
-//    echo "Не подключился к базе данных";
-//}
+function check_passwords($param1, $param2) 
+{
+    if ($param1 == $param2) {
+         echo "Пароли совпадают!";
+         return true;
+    } else {
+         echo "Пароли не совпадают";
+         return false;
+    }
+} 
 
-$login = $_POST['login']; #получаем логин с помощью метода POST все как и снизу
-$email = $_POST['email'];
-$password = $_POST['password'];
-$reppswd = $_POST['reppswd'];
-
-
-=======
->>>>>>> 1059182 (Изменил регистрацию)
-if ($password != $reppswd) {
-   echo "Пароли не совпадают!";
-}else{
-   echo "Пароли совпадают"; 
-   $login_check =$pdo->query("SELECT COUNT(*) FROM users WHERE login='$login'");
+function check_login($param1, $param2) 
+{
+   $login_check = $param2->query("SELECT COUNT(*) FROM users WHERE login='$param1'");
+   echo "Запрос к бд логин выполнен";
    $number_of_logins = $login_check->fetchColumn();
    echo "$number_of_logins";
-<<<<<<< HEAD
-   if ($nubmer_of_logins != 0) {
-       echo "Данный логин уже занят другим пользователем! Придумайте другой логин. ";
-   }else{
+   if ($number_of_logins == 0) {
        echo "Данный логин свободен!";
-//   $login_check = $pdo->prepare("SELECT COUNT(*) FROM users WHERE login='$login'");
-//   echo "Запрос подготовлен";
-//   $login_check->execute();
-//   echo "Запрос выполнен";
-//   $login_rows=$login_check->fetchColumn();
-//   echo "$login_rows";
-       $insrt = "INSERT INTO users (e_mail, login, password) VALUES ('$email', '$login', '$password');";
-       $result = $pdo->query($insrt);
-       echo "Данные вставлены";
-    }
-} 
-/*
-   // $email_check="SELECT count (*) as number_of_emails FROM users WHERE e_mail='$email'";
-    if ($login_check > 0) {
-	echo "Логин занят";
-    }else{
-	echo "Логин свободен" }
-=======
-   if ($number_of_logins != 0) {
-       echo "Данный логин уже занят другим пользователем! Придумайте другой логин. ";
+       return true;
    }else{
-       echo "Данный логин свободен!";
-       $email_check=$pdo->query("SELECT COUNT(*) FROM users WHERE e_mail='$email'");
-       $number_of_emails = $email_check->fetchColumn();
-       echo "$number_of_emails";
-       if ($number_of_emails !=0 ) {
-           echo "Данный e-mail уже занят!";
-       }else{
-	   $insrt = "INSERT INTO users (e_mail, login, password) VALUES ('$email', '$login', '$password');";
-           $result = $pdo->query($insrt);
-           echo "Данные вставлены";
-       }
+       echo "Данный логин уже занят другим пользователем! Придумайте другой логин. ";
+       return false;
+   }
+}
+
+function check_email($param1, $param2) 
+{
+   $email_check = $param1->query("SELECT COUNT(*) FROM users WHERE e_mail='$param2'");
+   $number_of_emails = $email_check->fetchColumn();
+   echo "$number_of_emails";
+   if ($number_of_emails == 0) {
+        echo "Данная почта свободна!";
+        return true;
+   } else {
+        echo "Данная почта уже использована для регистрации";
+        return false;
    }
 } 
-/*	echo "Логин свободен" }
->>>>>>> 1059182 (Изменил регистрацию)
-} 
-        header("Location: login_failed.php");
-        echo "Логин уже занят";
-    }elseif($pdo->query($login_check) = 0) {
-        echo "Логин не занят";
-    }
-}
-//	$email_check="SELECT * FROM users WHERE e_mail='$email'";
-	if ($pdo->query($email_check) > 0) {
-	header("Location: email_failed.php");
-	echo "email уже занят";
-    }else{ */
-//   $insrt = "INSERT INTO users (e_mail, login, password) VALUES ('$email', '$login', '$password');";
-//   $result = $pdo->query($insrt);
-//   echo "Данные вставлены";
-//}
-//}
 
+function insert_data_into_db($param1, $param2, $param3, $param4) {
+    $insrt = "INSERT INTO users (e_mail, login, password) VALUES ('$param2', '$param3', '$param4');";
+    $result = $param1->query($insrt);
+    echo "Регистрация прошла успешно!";
+}
+
+if (check_passwords($password, $reppswd)) {
+    echo "Пароли совпадают";
+    if (check_login($login, $pdo)) {
+       echo "Данный логин свободен"; 
+       if (check_email($pdo, $email)) {
+           echo "Данная почта свободна!";
+	   insert_data_into_db($pdo, $email, $login, $password);
+       } else {
+           echo "Почта занята!";
+           header("Location: email_failed.php");
+       }
+    } else {
+      echo "Данный логин занят";
+      header("Location: login_failed.php");
+    }
+} else {
+   echo "Не совпадают";
+   header("Location: different_passwords.php");
+}
+
+//echo "$password" . "$reppswd";
 ?>
 
